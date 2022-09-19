81b208be086ee378e701b0b1997caf1dd85e9c48
YashwanthLakkimsetti <96442644+YashwanthLakkimsetti@users.noreply.github.com>
2022-09-19 10:06:57 +0530

    Add files via upload

---
+++ Problemon19.java
@@ -0,0 +1,47 @@
+// Given an array a of integers of length n, find the nearest smaller number for every element such that the smaller element is on left side.If no small element present on the left print -1.
+
+// Example 1:
+
+// Input: n = 3
+// a = {1, 6, 2}
+// Output: -1 1 1
+// Explaination: There is no number at the 
+// left of 1. Smaller number than 6 and 2 is 1.
+// Example 2:
+
+// Input: n = 6
+// a = {1, 5, 0, 3, 4, 5}
+// Output: -1 1 -1 0 3 4
+// Explaination: Upto 3 it is easy to see 
+// the smaller numbers. But for 4 the smaller 
+// numbers are 1, 0 and 3. But among them 3 
+// is closest. Similary for 5 it is 4.
+// Your Task:
+// You do not need to read input or print anything. Your task is to complete the function leftSmaller() which takes n and a as input parameters and returns the list of smaller numbers.
+
+
+
+
+class Problemon19{
+    static List<Integer> leftSmaller(int n, int a[])
+    {
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        
+        
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && st.peek()>=a[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                al.add(-1);
+            }
+            else{
+                al.add(st.peek());
+            }
+            st.push(a[i]);
+        }
+        return al;
+    }
+}
+
